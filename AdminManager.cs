2ba5c83b87322510822adfd068866fdc2a2ad948
Manny <easweeney@wpi.edu>
2026-03-09 23:33:29 -0400

    CreateNewAccount
    now updateaccount is all that remains -- that and the sql dump

--- AdminManager.cs
+++ AdminManager.cs
@@ -8,12 +8,39 @@ public class adminManager {
     public adminManager() {}
 
     public void createNewAccount() {
-        
+        Console.Clear();
+
+        try {
+            Console.Write("Login:");
+            string? l = Console.ReadLine(); if (l == null) {l = "";}
+
+            Console.Write("PIN:");
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Holder's name:");
+            string? h = Console.ReadLine(); if (h == null) {h = "";}
+
+            Console.Write("Starting Balance:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            string ?a = "";
+            while (a != "y" && a != "n") {
+                Console.Write("Active? (Y/N):");
+                a = Console.ReadLine(); if (a == null) {a = "";}
+                a = a.ToLower();    
+            }      
+
+            int id = Dal.createAccount(h,l,p,b,a == "y");
+            Console.WriteLine("Account " + id + " Successfully Created.");   
+        } catch {
+            Console.WriteLine("Account Creation Failed."); 
+        }
+
     }
 
     public void deleteAccount() {
         Console.Clear();
-        Console.WriteLine("Enter the account number to which you want to delete:");
+        Console.Write("Enter the account number to which you want to delete:");
         try {
             int num = Convert.ToInt32(Console.ReadLine());
 
@@ -27,7 +54,7 @@ public class adminManager {
             string s = dt.Rows[0]["holder"].ToString();
             if (s == null) {s = "";}
 
-            Console.WriteLine("You are about to delete the account of " + s + ".\nRepeat the account number to proceed.");
+            Console.Write("You are about to delete the account of " + s + ".\nRepeat the account number to proceed.");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
             if (num == num2) {
@@ -45,6 +72,13 @@ public class adminManager {
     }
 
     public void updateAccount() {
+        Console.Clear();
+        Console.Write("Enter the Account Number:");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        DataTable dt = Dal.searchID(id);
+
+        //Print Standing Data
         
     }
 
@@ -104,7 +138,7 @@ public class adminManager {
             val = Console.ReadLine();
 
             switch (val) {
-                case "1": break;
+                case "1": createNewAccount(); break;
                 case "2": deleteAccount(); break;
                 case "3": break;
                 case "4": searchForAccount(); break;
